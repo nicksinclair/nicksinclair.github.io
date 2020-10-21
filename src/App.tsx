0f632ff7b183ddea26b38a8488abffcfb412a6a3
Nick Sinclair <nick.sinclair@itential.com>
2020-10-21 13:52:27 -0400

    Added routing for / and /about, created npm script for lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // Absolute imports
 import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 // Relative imports
 import Navbar from './components/Navbar';
@@ -7,19 +8,50 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Navbar />
+    <HashRouter basename="/">
+      <div className="App">
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
 
-      <header className="App-header">
-        <p>In Progress!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+        <Navbar />
+
+        <header className="App-header">
+          <p>In Progress!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </HashRouter>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
     </div>
   );
 };
